0f5c3022de0cabe41f2b9a54b140b1fa0fa5e6e7
Caio Machado <caio.student@outlook.com>
2021-04-14 00:26:37 -0300

    ajustes

--- Model/js/configAtividade1.js
+++ Model/js/configAtividade1.js
@@ -1,9 +1,20 @@
+var operadores = document.getElementsByClassName("operador");
 
-function operador() {
-    
+var indexOperador;
+var clique = false;
+function operador(id) {
+    indexOperador = id - 1;
+    clique = true
 }
 
 
-function selectedOperador() {
+function selectedOperador(id) {
+    if (clique){
+        if (id >= 11 || id <= 14) {
+            operadores[indexOperador].innerText = '';
+            operadores[indexOperador].innerHTML = `<img src="../../images/numbers/operadores/${id}.png" alt="">`;
 
+        }
+
+    }
 }
