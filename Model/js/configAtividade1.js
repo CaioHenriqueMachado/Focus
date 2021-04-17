289a5368e35835d9e300f1b0070b8d5d2fc724bc
Caio Machado <caio.student@outlook.com>
2021-04-16 21:11:39 -0300

    ajuste de tema

--- Model/js/configAtividade1.js
+++ Model/js/configAtividade1.js
@@ -8,34 +8,19 @@ var atividade = {
     id: 1,
     descricao: "",
     questao: {
-        1: {1: "",2: "",3: "",4: ""},
-        2: {1: "",2: "",3: "",4: ""},
-        3: {1: "",2: "",3: "",4: ""},
-        4: {1: "",2: "",3: "",4: ""},
-        5: {1: "",2: "",3: "",4: ""},
-        6: {1: "",2: "",3: "",4: ""},
-        7: {1: "",2: "",3: "",4: ""},
-        8: {1: "",2: "",3: "",4: ""},
-        9: {1: "",2: "",3: "",4: ""},
-        10: {1: "",2: "",3: "",4: ""},
+        1: { 1: "", 2: "", 3: "", 4: "" },
+        2: { 1: "", 2: "", 3: "", 4: "" },
+        3: { 1: "", 2: "", 3: "", 4: "" },
+        4: { 1: "", 2: "", 3: "", 4: "" },
+        5: { 1: "", 2: "", 3: "", 4: "" },
+        6: { 1: "", 2: "", 3: "", 4: "" },
+        7: { 1: "", 2: "", 3: "", 4: "" },
+        8: { 1: "", 2: "", 3: "", 4: "" },
+        9: { 1: "", 2: "", 3: "", 4: "" },
+        10: { 1: "", 2: "", 3: "", 4: "" },
     }
 }
 
-
-
-// console.log(atividade.questao[1][1]);
-
-// for (var index in atividade.questao[1][1]) {
-
-//     console.log(atividade.questao[1][1].substr(index,1));
-// }
-
-// console.log(atividade.questoes[0][0].substr(2,1));
-
-// atividade.questoes[0] = ["1"]
-// atividade.questao[1][0] = "1";
-
-// console.log(atividade.questao[1][1]);
 
 var indexQuestion;
 var indexBox;
@@ -55,8 +40,8 @@ function selectedFigure(id) {
                 limitNumberBox(indexQuestion, indexBox, id, 2);
 
                 for (var index in atividade.questao[indexQuestion][indexBox]) {
-                    var id = atividade.questao[indexQuestion][indexBox].substr(index,1)
-                    box1[indexQuestion - 1].innerHTML += `<img src="../../images/numbers/monsters/${id}.png" alt="">`;
+                    var id = atividade.questao[indexQuestion][indexBox].substr(index, 1)
+                    box1[indexQuestion - 1].innerHTML += `<img src="../../images/numbers/${theme}/${id}.png" alt="">`;
                 }
                 // console.log(atividade.questao[indexQuestion]);
             }
@@ -79,8 +64,8 @@ function selectedFigure(id) {
                 limitNumberBox(indexQuestion, indexBox, id, 2);
 
                 for (var index in atividade.questao[indexQuestion][indexBox]) {
-                    var id = atividade.questao[indexQuestion][indexBox].substr(index,1)
-                    box3[indexQuestion - 1].innerHTML += `<img src="../../images/numbers/monsters/${id}.png" alt="">`;
+                    var id = atividade.questao[indexQuestion][indexBox].substr(index, 1)
+                    box3[indexQuestion - 1].innerHTML += `<img src="../../images/numbers/${theme}/${id}.png" alt="">`;
                 }
                 // console.log(atividade.questao[indexQuestion]);
             }
@@ -92,8 +77,8 @@ function selectedFigure(id) {
                 limitNumberBox(indexQuestion, indexBox, id, 3);
 
                 for (var index in atividade.questao[indexQuestion][indexBox]) {
-                    var id = atividade.questao[indexQuestion][indexBox].substr(index,1)
-                    resultado[indexQuestion - 1].innerHTML += `<img src="../../images/numbers/monsters/${id}.png" alt="">`;
+                    var id = atividade.questao[indexQuestion][indexBox].substr(index, 1)
+                    resultado[indexQuestion - 1].innerHTML += `<img src="../../images/numbers/${theme}/${id}.png" alt="">`;
                 }
                 // console.log(atividade.questao[indexQuestion]);
             }
@@ -104,24 +89,20 @@ function selectedFigure(id) {
 
 function limitNumberBox(question, box, number, limit) {
     var quantidade = atividade.questao[question][box].length
-    if ( quantidade == 0 || quantidade == limit ){
+    if (quantidade == 0 || quantidade == limit) {
         atividade.questao[question][box] = String(number);
-    }else{
+    } else {
         atividade.questao[question][box] += String(number);
     }
 }
-
-
-
-
 
 function validationConfig() {
     var inputs = document.getElementsByTagName("input");
     var campoVazio = false;
     var indexVazio = 0;
 
-    for (var i = 0; i <= 2; i++){
-        if(inputs[i].value.length == 0) {
+    for (var i = 0; i <= 2; i++) {
+        if (inputs[i].value.length == 0) {
             campoVazio = true;
             indexVazio = i;
             break;
@@ -130,19 +111,20 @@ function validationConfig() {
 
     if (campoVazio) {
         alert(`O campo ${inputs[indexVazio].name} deve ser preenchido !`);
-    }else {
-        window.location.href = "#Question1"
-    }
-    
-}
+    } else {
+        alert(`Você finalizou essa atividade!`);
+        window.location.href = `www.uol.com.br`; 
 
+    }
+
+}
 
 function validationQuestion(question) {
     var campoVazio = false;
     var indexVazio = 0;
 
-    for (var i=1; i <= 4; i++) {
-        if (atividade.questao[question][i].length == 0){
+    for (var i = 1; i <= 4; i++) {
+        if (atividade.questao[question][i].length == 0) {
             campoVazio = true;
             indexVazio = i;
             break;
@@ -150,20 +132,30 @@ function validationQuestion(question) {
     }
 
     if (campoVazio) {
-        if (indexVazio == 2){
+        if (indexVazio == 2) {
             alert(`Um operador deve ser escolhido na questão ${question} !`);
-        }else if(indexVazio == 4) {
+        } else if (indexVazio == 4) {
             alert(`Um Resultado deve ser escolhido na questão ${question} !`);
-        }else {
+        } else {
             alert(`Um Número deve ser escolhido na questão ${question} !`);
         }
 
-    }else {
-        if (question != 10){
+    } else {
+        if (question != 10) {
             window.location.href = `#Question${question + 1}`;
+        }else {
+            window.location.href = `#configuracoes`; 
         }
-        
     }
+}
 
-    
+var theme = "monsters";
+
+
+function selectTheme(type) {
+    var images = document.getElementsByClassName("imageTheme");
+
+    for(var i = 0; i <= 9; i++){
+        images[i].src = `../../images/numbers/${type}/${i}.png`;
+    }
 }
